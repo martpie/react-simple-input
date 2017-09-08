a704fbd107f5035e31b6bbc0ed93a24390e5fd5c
KeitIG <pierre.de.la.martiniere@gmail.com>
2017-09-08 14:23:57 +0200

    Re-indent files with two whitespaces

--- js/ReactSimpleInput.jsx
+++ js/ReactSimpleInput.jsx
@@ -4,120 +4,120 @@ import React, { Component } from 'react';
 
 class Input extends Component {
 
-    constructor(props) {
+  constructor(props) {
 
-        super(props);
+    super(props);
 
-        this.state = {
-            isEmpty : this.props.defaultValue !== ''
-        }
-
-        this.onClick = this.onClick.bind(this);
-
-        const date = Date.now();
-
-        this.timeoutsDates = {
-            onChange: date,
-            onKeyUp: date,
-            onKeyDown: date,
-            onKeyPress: date
-        };
-
-        this.timeouts = {};
+    this.state = {
+      isEmpty : this.props.defaultValue !== ''
     }
 
-    render() {
+    this.onClick = this.onClick.bind(this);
 
-        let clearButton = false;
+    const date = Date.now();
 
-        if(this.props.clearButton && this.state.isEmpty) {
-            clearButton = <div className='react-simple-input-clear' onClick={ () => { this.clear() } }>&times;</div>
-        }
+    this.timeoutsDates = {
+      onChange: date,
+      onKeyUp: date,
+      onKeyDown: date,
+      onKeyPress: date
+    };
 
-        return (
-            <div className={ 'react-simple-input-container ' + this.props.classNameContainer }>
-                <input
-                    type='text'
-                    defaultValue={ this.props.defaultValue }
-                    className={ 'react-simple-input ' + this.props.className }
-                    placeholder={ this.props.placeholder }
-                    ref='input'
-                    onChange={ (e) => { this.delayedEvent(e, 'onChange'); } }
-                    onKeyDown={ (e) => { this.delayedEvent(e, 'onKeyDown'); } }
-                    onKeyUp={ (e) => { this.delayedEvent(e, 'onKeyUp'); } }
-                    onKeyPress={ (e) => { this.delayedEvent(e, 'onKeyPress'); } }
-                    onClick={ this.onClick }
-                />
-                { clearButton }
-            </div>
-        );
+    this.timeouts = {};
+  }
+
+  render() {
+
+    let clearButton = false;
+
+    if(this.props.clearButton && this.state.isEmpty) {
+      clearButton = <div className='react-simple-input-clear' onClick={ () => { this.clear() } }>&times;</div>
     }
 
-    onClick(e) {
+    return (
+      <div className={ 'react-simple-input-container ' + this.props.classNameContainer }>
+        <input
+          type='text'
+          defaultValue={ this.props.defaultValue }
+          className={ 'react-simple-input ' + this.props.className }
+          placeholder={ this.props.placeholder }
+          ref='input'
+          onChange={ (e) => { this.delayedEvent(e, 'onChange'); } }
+          onKeyDown={ (e) => { this.delayedEvent(e, 'onKeyDown'); } }
+          onKeyUp={ (e) => { this.delayedEvent(e, 'onKeyUp'); } }
+          onKeyPress={ (e) => { this.delayedEvent(e, 'onKeyPress'); } }
+          onClick={ this.onClick }
+        />
+        { clearButton }
+      </div>
+    );
+  }
 
-        e.persist();
+  onClick(e) {
 
-        if(this.props.selectOnClick) this.refs.input.select();
+    e.persist();
 
-        this.props.onClick(e);
+    if(this.props.selectOnClick) this.refs.input.select();
+
+    this.props.onClick(e);
+  }
+
+  delayedEvent(e, type) {
+
+    e.persist();
+
+    const self = this;
+    const now  = Date.now();
+    const timeout = this.props.eventsTimeouts[type] || 0;
+
+    if(now - this.timeoutsDates[type] < timeout) {
+      clearTimeout(this.timeouts[type]);
     }
 
-    delayedEvent(e, type) {
+    this.timeoutsDates[type] = now;
 
-        e.persist();
+    self.setState({ isEmpty : e.currentTarget.value !== '' });
 
-        const self = this;
-        const now  = Date.now();
-        const timeout = this.props.eventsTimeouts[type] || 0;
+    this.timeouts[type] = setTimeout(function() {
+      self.props[type](e);
+    }, timeout);
+  }
 
-        if(now - this.timeoutsDates[type] < timeout) {
-            clearTimeout(this.timeouts[type]);
-        }
-
-        this.timeoutsDates[type] = now;
-
-        self.setState({ isEmpty : e.currentTarget.value !== '' });
-
-        this.timeouts[type] = setTimeout(function() {
-            self.props[type](e);
-        }, timeout);
-    }
-
-    clear() {
-        this.refs.input.value = '';
-        const event = new Event('input', { bubbles: true });
-        this.refs.input.dispatchEvent(event);
-    }
+  clear() {
+    this.refs.input.value = '';
+    const event = new Event('input', { bubbles: true });
+    this.refs.input.dispatchEvent(event);
+  }
 }
 
 Input.defaultProps = {
-        className          : '',
-        classNameContainer : '',
-        defaultValue       : '',
-        placeholder        : '',
-        clearButton        : false,
-        selectOnClick      : false,
-        eventsTimeouts     : {},
-        onChange           : () => {},
-        onKeyDown          : () => {},
-        onKeyUp            : () => {},
-        onKeyPress         : () => {},
-        onClick            : () => {}
+  className          : '',
+  classNameContainer : '',
+  defaultValue       : '',
+  placeholder        : '',
+  clearButton        : false,
+  selectOnClick      : false,
+  eventsTimeouts     : {},
+  onChange           : () => {},
+  onKeyDown          : () => {},
+  onKeyUp            : () => {},
+  onKeyPress         : () => {},
+  onClick            : () => {}
 };
 
 Input.propTypes = {
-    className          : React.PropTypes.string,
-    classNameContainer : React.PropTypes.string,
-    defaultValue       : React.PropTypes.string,
-    placeholder        : React.PropTypes.string,
-    clearButton        : React.PropTypes.bool,
-    selectOnClick      : React.PropTypes.bool,
-    eventsTimeouts     : React.PropTypes.object,
-    onChange           : React.PropTypes.func,
-    onKeyDown          : React.PropTypes.func,
-    onKeyUp            : React.PropTypes.func,
-    onKeyPress         : React.PropTypes.func,
-    onClick            : React.PropTypes.func
+  className          : React.PropTypes.string,
+  classNameContainer : React.PropTypes.string,
+  defaultValue       : React.PropTypes.string,
+  placeholder        : React.PropTypes.string,
+  clearButton        : React.PropTypes.bool,
+  selectOnClick      : React.PropTypes.bool,
+  eventsTimeouts     : React.PropTypes.object,
+  onChange           : React.PropTypes.func,
+  onKeyDown          : React.PropTypes.func,
+  onKeyUp            : React.PropTypes.func,
+  onKeyPress         : React.PropTypes.func,
+  onClick            : React.PropTypes.func
 };
 
 export default Input;
